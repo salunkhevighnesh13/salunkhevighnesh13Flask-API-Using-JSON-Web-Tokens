a5e4cfa79d2d92c1fca87dc88c58ba20714cc638
salunkhevighnesh13 <56196081+salunkhevighnesh13@users.noreply.github.com>
2020-09-29 10:15:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify, request, make_response
+import jwt
+import datetime
+from functools import wraps
+
+app = Flask(__name__)
+app.config['SECRET-KEY'] = 'thisisthesecretkey'
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')
+        if not token:
+            return jsonify({'message': 'token is missing'}), 403
+        try:
+            data = jwt.decode(token, app.config['SECRET-KEY'])
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 403
+        return f(*args, **kwargs)
+
+    return decorated
+
+
+@app.route("/unprotected")
+def unprotected():
+    return jsonify({'message': 'Anyone can view this'})
+
+
+@app.route("/protected")
+@token_required
+def protected():
+    return jsonify({'message': 'This is only available for people with valid token'})
+
+
+@app.route("/login")
+def login():
+    auth = request.authorization
+    if auth and auth.password == 'password':
+        token = jwt.encode({'user': auth.username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=15)},
+                           app.config['SECRET-KEY'])
+        return jsonify({'token': token.decode('UTF-8')})
+    return make_response("could not verify", 401, {"WWW-Authenticate": "Basic realm='Login Required'"})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
